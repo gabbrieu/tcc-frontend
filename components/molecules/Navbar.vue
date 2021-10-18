91c3e10a3b5689de80f1683af6d037c734d712f0
gabbrieu <gabrielhmendes@hotmail.com>
2021-10-17 23:02:50 -0300

    feat: adding navbar and logo components and changing colors in tailwind

---
+++ components/molecules/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav class="navbar-color h-14 flex justify-between items-center">
+    <Logo />
+    <p class="font-bold text-white pr-9">Erlaine</p>
+  </nav>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({})
+</script>
+
+<style scoped>
+.navbar-color {
+  background: #3f53fd;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,23 @@
 <template>
-  <Tutorial/>
+  <div>
+    <Navbar />
+    <main class="bg-background h-screen">
+      <button
+        class="
+          mt-4
+          mb-4
+          ml-9
+          p-2
+          rounded-lg
+          border-blue border
+          text-blue
+          font-semibold
+        "
+      >
+        + Novo cliente
+      </button>
+    </main>
+  </div>
 </template>
 
 <script lang="ts">
@@ -7,3 +25,9 @@ import Vue from 'vue'
 
 export default Vue.extend({})
 </script>
+
+<style scoped>
+.border {
+  border-width: 1px;
+}
+</style>
