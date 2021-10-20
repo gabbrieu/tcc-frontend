1c434a1e3c7bdc9ec76a54bb03ef9d8d4a3d2ae7
gabbrieu <gabrielhmendes@hotmail.com>
2021-10-19 23:42:39 -0300

    feat: adding kanban column and card components

---
+++ components/atoms/Card.vue
@@ -0,0 +1,26 @@
+<template>
+  <draggable class="list-group" :list="array" group="tasks">
+    <div
+      class="list-group-item bg-white pl-2 mt-2 ml-3 mr-3"
+      v-for="element in array"
+      :key="element.name"
+    >
+      {{ element.name }}
+    </div>
+  </draggable>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import draggable from 'vuedraggable'
+
+export default Vue.extend({
+  components: {
+    draggable,
+  },
+
+  props: {
+    array: Array,
+  },
+})
+</script>
