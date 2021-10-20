77a28b2da51519fdd7ba64cd14c55899a2878e18
gabbrieu <gabrielhmendes@hotmail.com>
2021-10-20 18:15:15 -0300

    refactor: adding animation to draggable and set global font

--- components/atoms/Card.vue
+++ components/atoms/Card.vue
@@ -1,13 +1,13 @@
 <template>
-  <draggable class="list-group" :list="array" group="tasks">
-    <div
-      class="list-group-item bg-white pl-2 mt-2 ml-3 mr-3"
-      v-for="element in array"
-      :key="element.name"
-    >
-      {{ element.name }}
-    </div>
-  </draggable>
+	<draggable class="list" :list="array" group="tasks" animation="250">
+		<div
+			class="list-group-item bg-white pl-2 mt-2 ml-3 mr-3"
+			v-for="element in array"
+			:key="element.name"
+		>
+			{{ element.name }}
+		</div>
+	</draggable>
 </template>
 
 <script lang="ts">
@@ -15,12 +15,11 @@ import Vue from 'vue'
 import draggable from 'vuedraggable'
 
 export default Vue.extend({
-  components: {
-    draggable,
-  },
-
-  props: {
-    array: Array,
-  },
+	components: {
+		draggable,
+	},
+	props: {
+		array: Array,
+	},
 })
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,92 +1,92 @@
 <template>
-  <div>
-    <Navbar />
-    <main>
-      <button
-        class="
-          mt-4
-          mb-4
-          ml-9
-          pt-2
-          pb-2
-          rounded-lg
-          border-blue border
-          text-blue
-          font-semibold
-          pr-4
-          pl-4
-        "
-      >
-        + Novo cliente
-      </button>
-      <div class="flex flex-row">
-        <div class="flex flex-col flex-1">
-          <KanbanColumn
-            title="Cliente em potencial"
-            :column="potentialClient"
-          />
-        </div>
+	<div>
+		<Navbar />
+		<main>
+			<button
+				class="
+					mt-4
+					mb-4
+					ml-9
+					pt-2
+					pb-2
+					rounded-lg
+					border-blue border
+					text-blue
+					font-semibold
+					pr-4
+					pl-4
+				"
+			>
+				+ Novo cliente
+			</button>
+			<div class="flex flex-row">
+				<div class="flex flex-col flex-1">
+					<KanbanColumn
+						title="Cliente em potencial"
+						:column="potentialClient"
+					/>
+				</div>
 
-        <div class="flex flex-col flex-1">
-          <KanbanColumn title="Contato realizado" :column="contactMade" />
-        </div>
+				<div class="flex flex-col flex-1">
+					<KanbanColumn title="Contato realizado" :column="contactMade" />
+				</div>
 
-        <div class="flex flex-col flex-1">
-          <KanbanColumn title="Visita agendada" :column="scheduledVisit" />
-        </div>
+				<div class="flex flex-col flex-1">
+					<KanbanColumn title="Visita agendada" :column="scheduledVisit" />
+				</div>
 
-        <div class="flex flex-col flex-1">
-          <KanbanColumn
-            title="Negócio em andamento"
-            :column="businessInProgress"
-          />
-        </div>
+				<div class="flex flex-col flex-1">
+					<KanbanColumn
+						title="Negócio em andamento"
+						:column="businessInProgress"
+					/>
+				</div>
 
-        <div class="flex flex-col flex-1">
-          <KanbanColumn title="Finalizados" :column="done" />
-        </div>
-      </div>
-    </main>
-  </div>
+				<div class="flex flex-col flex-1">
+					<KanbanColumn title="Finalizados" :column="done" />
+				</div>
+			</div>
+		</main>
+	</div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
 export default Vue.extend({
-  data() {
-    return {
-      newTasks: '',
-      potentialClient: [
-        { name: 'Cliente em potencial 1' },
-        { name: 'Cliente em potencial 2' },
-        { name: 'Cliente em potencial 3' },
-        { name: 'Cliente em potencial 4' },
-      ],
-      contactMade: [
-        { name: 'Contato realizado 1' },
-        { name: 'Contato realizado 2' },
-        { name: 'Contato realizado 3' },
-        { name: 'Contato realizado 4' },
-        { name: 'Contato realizado 5' },
-      ],
-      scheduledVisit: [{ name: 'Visita agendada 1' }],
-      businessInProgress: [
-        { name: 'Negócio em andamento 1' },
-        { name: 'Negócio em andamento 2' },
-      ],
-      done: [
-        { name: 'Finalizados 1' },
-        { name: 'Finalizados 2' },
-        { name: 'Finalizados 3' },
-      ],
-    }
-  },
+	data() {
+		return {
+			newTasks: '',
+			potentialClient: [
+				{ name: 'Cliente em potencial 1' },
+				{ name: 'Cliente em potencial 2' },
+				{ name: 'Cliente em potencial 3' },
+				{ name: 'Cliente em potencial 4' },
+			],
+			contactMade: [
+				{ name: 'Contato realizado 1' },
+				{ name: 'Contato realizado 2' },
+				{ name: 'Contato realizado 3' },
+				{ name: 'Contato realizado 4' },
+				{ name: 'Contato realizado 5' },
+			],
+			scheduledVisit: [{ name: 'Visita agendada 1' }],
+			businessInProgress: [
+				{ name: 'Negócio em andamento 1' },
+				{ name: 'Negócio em andamento 2' },
+			],
+			done: [
+				{ name: 'Finalizados 1' },
+				{ name: 'Finalizados 2' },
+				{ name: 'Finalizados 3' },
+			],
+		}
+	},
 })
 </script>
 
 <style scoped>
 .border {
-  border-width: 1px;
+	border-width: 1px;
 }
 </style>
